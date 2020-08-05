6e72d873b49836f9fb1550a6d1bc5a945a3f5a12
Sergei Sarapov <sarapov.ss@gmail.com>
2020-08-05 11:45:19 +0300

    Updates

---
+++ precache-manifest.7f2d952b7226d5b2aa1c0f7e4c358b55.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d9242b036a020ce146c2fe353d5ea08",
+    "url": "/todo-react/index.html"
+  },
+  {
+    "revision": "fac75d0256e213f7430e",
+    "url": "/todo-react/static/css/main.ba8ed9fd.chunk.css"
+  },
+  {
+    "revision": "a71d557632e62c65a148",
+    "url": "/todo-react/static/js/2.1409cdef.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-react/static/js/2.1409cdef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fac75d0256e213f7430e",
+    "url": "/todo-react/static/js/main.3edbff66.chunk.js"
+  },
+  {
+    "revision": "1614377f578425b5d648",
+    "url": "/todo-react/static/js/runtime-main.92cd31f8.js"
+  }
+]);
